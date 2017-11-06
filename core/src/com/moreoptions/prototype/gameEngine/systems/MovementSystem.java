d80f2c0aa0c5b33a0a3c57be70f93b492565ed56
Miso <denweisenseel@gmail.com>
2017-11-06 23:28:58 +0100

    More utility for collisionchecking

--- core/src/com/moreoptions/prototype/gameEngine/systems/MovementSystem.java
+++ core/src/com/moreoptions/prototype/gameEngine/systems/MovementSystem.java
@@ -16,7 +16,8 @@ import java.util.List;
 /**
  * The job of this movement-system is to:
  * -Move Entities
- * -Resolve Collisions with immovable entities, entities that _do_not_ move. makes us unable to pass bounds, or run over rocks.
+ * -Resolve Collisions with tiles
+ * -Resolves Collision with nonmoving entities.
  *
  * Maybe implement a spacial map to not iterate x²
  *
@@ -27,6 +28,7 @@ public class MovementSystem extends EntitySystem {
     ComponentMapper<PositionComponent>  posMapper   = ComponentMapper.getFor(PositionComponent.class);
     ComponentMapper<VelocityComponent>  velMapper   = ComponentMapper.getFor(VelocityComponent.class);
     ComponentMapper<CircleCollisionComponent> cMapper = ComponentMapper.getFor(CircleCollisionComponent.class);
+    ComponentMapper<SquareCollisionComponent> sqMapper = ComponentMapper.getFor(SquareCollisionComponent.class);
 
     Family posColl = Family.all(PositionComponent.class, CollisionComponent.class, VelocityComponent.class, CircleCollisionComponent.class).exclude(TileComponent.class).get();
     Family blockedTilesFamily = Family.all(BlockedTileComponent.class).get();
@@ -52,15 +54,21 @@ public class MovementSystem extends EntitySystem {
             PositionComponent pos = posMapper.get(e);
             CollisionComponent col= colMapper.get(e);
 
+
             //Record previous position for later collision resolving
             col.setOldX(pos.getX());
             col.setOldY(pos.getY());
 
             pos.setX(pos.getX() + vel.getVelX() * deltaTime);
+            if(sqMapper.has(e)) sqMapper.get(e).getHitbox().setPosition(pos.getX(),pos.getY());
+            if(cMapper.has(e)) cMapper.get(e).getHitbox().setPosition(pos.getX(),pos.getY());
             resolveXCollision(e,col.getOldX(), col.getOldY());
 
 
             pos.setY(pos.getY() + vel.getVelY() * deltaTime);
+
+            if(sqMapper.has(e)) sqMapper.get(e).getHitbox().setPosition(pos.getX(),pos.getY());
+            if(cMapper.has(e)) cMapper.get(e).getHitbox().setPosition(pos.getX(),pos.getY());
             resolveYCollision(e,col.getOldX(), col.getOldY());
 
             //Add smooth edge movement
@@ -71,7 +79,7 @@ public class MovementSystem extends EntitySystem {
     private void resolveXCollision(Entity e, float x,float y) {
             for(Entity t : getEngine().getEntitiesFor(blockedTilesFamily)) {
                 try {
-                    float r = CollisionUtil.getXOverlap(EntityTools.getEntityHitbox(e), EntityTools.getTileHitbox(t),x,y);
+                    float r = CollisionUtil.getXOverlap(EntityTools.getCircleHitbox(e), EntityTools.getSquareHitbox(t),x,y);
                     updateEntityXPosition(e,r);
                 } catch (Exception e1) {
                     e1.printStackTrace();
@@ -82,7 +90,7 @@ public class MovementSystem extends EntitySystem {
     private void resolveYCollision(Entity e, float x,float y) {
         for(Entity t : getEngine().getEntitiesFor(blockedTilesFamily)) {
             try {
-                float r = CollisionUtil.getYOverlap(EntityTools.getEntityHitbox(e), EntityTools.getTileHitbox(t),x,y);
+                float r = CollisionUtil.getYOverlap(EntityTools.getCircleHitbox(e), EntityTools.getSquareHitbox(t),x,y);
                 updateEntityYPosition(e, r);
             } catch (Exception e1) {
                 e1.printStackTrace();
--- core/src/com/moreoptions/prototype/gameEngine/util/EntityTools.java
+++ core/src/com/moreoptions/prototype/gameEngine/util/EntityTools.java
@@ -4,8 +4,10 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
+import com.moreoptions.prototype.gameEngine.components.CircleCollisionComponent;
 import com.moreoptions.prototype.gameEngine.components.CollisionComponent;
 import com.moreoptions.prototype.gameEngine.components.PositionComponent;
+import com.moreoptions.prototype.gameEngine.components.SquareCollisionComponent;
 import javafx.geometry.Pos;
 
 /**
@@ -13,19 +15,13 @@ import javafx.geometry.Pos;
  */
 public class EntityTools {
 
-    public static Rectangle getTileHitbox(Entity e) throws Exception {
-        CollisionComponent c = e.getComponent(CollisionComponent.class);
-        if(c.getShape() != CollisionComponent.Shape.RECTANGLE) throw new Exception("Not a rectangle hitbox");
-        PositionComponent p = e.getComponent(PositionComponent.class);
-
-        return new Rectangle(p.getX(),p.getY(),c.getSize(),c.getSize());
+    public static Rectangle getSquareHitbox(Entity e) throws Exception {
+        SquareCollisionComponent s = e.getComponent(SquareCollisionComponent.class);
+        return s.getHitbox();
     }
 
-    public static Circle getEntityHitbox(Entity e) throws Exception {
-        CollisionComponent c = e.getComponent(CollisionComponent.class);
-        if(c.getShape() != CollisionComponent.Shape.CIRCLE) throw new Exception("Not a circle hitbox");
-        PositionComponent p = e.getComponent(PositionComponent.class);
-
-        return new Circle(p.getX(),p.getY(),c.getSize());
+    public static Circle getCircleHitbox(Entity e) throws Exception {
+        CircleCollisionComponent s = e.getComponent(CircleCollisionComponent.class);
+        return s.getHitbox();
     }
 }
