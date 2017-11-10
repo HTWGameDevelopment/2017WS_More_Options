c5ed57651f3eb82240ee1a832d5bb86c6ba592ee
Frankiejones <Andaloria@gmx.de>
2017-11-10 16:27:50 +0100

    PathFinder overhaul: More Javadoc and smaller methods. Now also has a Constructor and is better accessible.

--- core/src/pathfinder/PathFinder.java
+++ core/src/pathfinder/PathFinder.java
@@ -9,7 +9,7 @@ import java.util.PriorityQueue;
  */
 public class PathFinder {
 
-    //TODO: change X and Y axis, make smaller methods, assign premade arrays of blocked tiles and generally try to make this better
+    //TODO: change X and Y axis, make smaller methods, assign premade arrays of blocked tiles and make some more tests
 
     // costs are chosen based on the characteristics of a triangle --> diagonal has higher cost than vertical or horizontal
     private static final int DIAGONAL_COST = 14;
@@ -30,6 +30,20 @@ public class PathFinder {
     // target of the algorithm
     private static int targetX;
     private static int targetY;
+
+    private static int width;
+    private static int height;
+
+    public PathFinder(int width, int height, int startX, int startY, int targetX, int targetY, int[][] blocked) {
+        this.width = width;
+        this.height = height;
+        this.startX = startX;
+        this.startY = startY;
+        this.targetX = targetX;
+        this.targetY = targetY;
+
+        testPathFinder(blocked);
+    }
 
     /**
      * sets a coordinate inside the "room" to be impassable
@@ -87,7 +101,7 @@ public class PathFinder {
     /**
      * actual A* algorithm to find the best path
      */
-    public static void findPath() {
+    private static void findPath(Cell[][] grid, int startX, int startY, int targetX, int targetY) {
         open.add(grid[startX][startY]);
 
         Cell current;
@@ -105,54 +119,81 @@ public class PathFinder {
                 return;
             }
 
-            Cell target;
+            updateCostForCells(grid, current);
+        }
+    }
 
-            // check for not left border
-            if (current.getX() - 1 >= 0) {
-                target = grid[current.getX() - 1][current.getY()];
-                updateCost(current, target, current.getFinalCost() + V_H_COST);
+    private static void updateCostForCells(Cell[][] grid, Cell current) {
+        // check for not left border and update the costs
+        updateCostNotLeft(grid, current);
 
-                // check for not bottom cell
-                if (current.getY() - 1 >= 0) {
-                    target = grid[current.getX() - 1][current.getY() - 1];
-                    updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
-                }
+        // check for not bottom border and update the costs
+        updateCostNotBottom(grid, current);
 
-                // check for not top cell
-                if (current.getY() + 1 < grid[0].length) {
-                    target = grid[current.getX() - 1][current.getY() + 1];
-                    updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
-                }
-            }
+        // check for not top border
+        updateCostNotTop(grid, current);
 
-            // check for not bottom border
+        // check for not right border
+        updateCostNotRight(grid, current);
+    }
+
+    private static void updateCostNotRight(Cell[][] grid, Cell current) {
+        Cell target;
+
+        if (current.getX() + 1 < grid.length) {
+            target = grid[current.getX() + 1][current.getY()];
+            updateCost(current, target, current.getFinalCost() + V_H_COST);
+
+            // check for not bottom cell
             if (current.getY() - 1 >= 0) {
-                target = grid[current.getX()][current.getY() - 1];
-                updateCost(current, target, current.getFinalCost() + V_H_COST);
+                target = grid[current.getX() + 1][current.getY() - 1];
+                updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
             }
 
-            // check for not top border
+            // check for not top cell
             if (current.getY() + 1 < grid[0].length) {
-                target = grid[current.getX()][current.getY() + 1];
-                updateCost(current, target, current.getFinalCost() + V_H_COST);
+                target = grid[current.getX() + 1][current.getY() + 1];
+                updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
+            }
+        }
+    }
+
+    private static void updateCostNotTop(Cell[][] grid, Cell current) {
+        Cell target;
+
+        if (current.getY() + 1 < grid[0].length) {
+            target = grid[current.getX()][current.getY() + 1];
+            updateCost(current, target, current.getFinalCost() + V_H_COST);
+        }
+
+    }
+
+    private static void updateCostNotBottom(Cell[][] grid, Cell current) {
+        Cell target;
+
+        if (current.getY() - 1 >= 0) {
+            target = grid[current.getX()][current.getY() - 1];
+            updateCost(current, target, current.getFinalCost() + V_H_COST);
+        }
+    }
+
+    private static void updateCostNotLeft(Cell[][] grid, Cell current) {
+        Cell target;
+
+        if (current.getX() - 1 >= 0) {
+            target = grid[current.getX() - 1][current.getY()];
+            updateCost(current, target, current.getFinalCost() + V_H_COST);
+
+            // check for not bottom cell
+            if (current.getY() - 1 >= 0) {
+                target = grid[current.getX() - 1][current.getY() - 1];
+                updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
             }
 
-            // check for not right border
-            if (current.getX() + 1 < grid.length) {
-                target = grid[current.getX() + 1][current.getY()];
-                updateCost(current, target, current.getFinalCost() + V_H_COST);
-
-                // check for not bottom cell
-                if (current.getY() - 1 >= 0) {
-                    target = grid[current.getX() + 1][current.getY() - 1];
-                    updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
-                }
-
-                // check for not top cell
-                if (current.getY() + 1 < grid[0].length) {
-                    target = grid[current.getX() + 1][current.getY() + 1];
-                    updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
-                }
+            // check for not top cell
+            if (current.getY() + 1 < grid[0].length) {
+                target = grid[current.getX() - 1][current.getY() + 1];
+                updateCost(current, target, current.getFinalCost() + DIAGONAL_COST);
             }
         }
     }
@@ -160,15 +201,9 @@ public class PathFinder {
     /**
      * prints the map to the shell before A* is used and then after it it used
      * also shows the path used
-     * @param width width of the grid
-     * @param height height of the grid
-     * @param startX x-coordinate of the starting point
-     * @param startY y-coordinate of the starting point
-     * @param targetX x-coordinate of the target point
-     * @param targetY y-coordinate of the target point
      * @param blocked collection of blocked tiles
      */
-    public static void testPathFinder(int width, int height, int startX, int startY, int targetX, int targetY, int[][] blocked) {
+    public static void testPathFinder(int[][] blocked) {
         // reset
 
         grid = new Cell[width][height];
@@ -186,12 +221,7 @@ public class PathFinder {
         setTarget(targetX, targetY);
 
         // initialize cells with cost
-        for (int i = 0; i < width; ++i) {
-            for (int j = 0; j < height; ++j) {
-                grid[i][j] = new Cell(i, j);
-                grid[i][j].setHeuristicCost(Math.abs(i - targetX) + Math.abs(j - targetY));
-            }
-        }
+        initializeMap(grid, width, height);
 
         grid[startX][startY].setFinalCost(0);
 
@@ -201,41 +231,26 @@ public class PathFinder {
         }
 
         // display initial map
-        System.out.println("Grid: ");
-        for (int i = 0; i < width; ++i) {
-            for (int j = 0; j < height; ++j) {
-                if (i == startX && j == startY) {
-                    System.out.print("SO  ");
-                } else if (i == targetX && j == targetY) {
-                    System.out.print("TG  ");
-                } else if (grid[i][j] != null) {
-                    System.out.printf("%-3d ", 0);
-                } else {
-                    System.out.print("BL  ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
+        printMap(width, height, startX, startY, targetX, targetY);
 
         // search best path
-        findPath();
+        findPath(grid, startX, startY, targetX, targetY); 
 
-
+        // print the map with the values
         System.out.println("\nValues of the Cells: ");
 
-        for (int i = 0; i < width; ++i) {
-            for (int j = 0; j < height; ++j) {
-                if (grid[i][j] != null) {
-                    System.out.printf("%-3d ", grid[i][j].getFinalCost());
-                } else {
-                    System.out.print("BL  ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
+        printMapWithValues(width, height, grid);
 
+        printPath(closed, targetX, targetY);
+    }
+
+    /**
+     * prints the path from the start to the target found by the A* algorithm
+     * @param closed the nodes taken
+     * @param targetX x-coordinate of the target location
+     * @param targetY y-coordinate of the target location
+     */
+    private static void printPath(boolean[][] closed, int targetX, int targetY) {
         if (closed[targetX][targetY]) {
             // trace path
 
@@ -253,9 +268,71 @@ public class PathFinder {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        testPathFinder(5, 5, 0, 0, 0, 4, new int[][]{{2,2}, {1,1}, {3,3}, {0, 3}});
+    /**
+     * prints a grid with the calculated final costs
+     * @param width width of the grid
+     * @param height height of the grid
+     * @param grid the map the values will be printed on
+     */
+    private static void printMapWithValues(int width, int height, Cell[][] grid) {
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                if (grid[i][j] != null) {
+                    System.out.printf("%-3d ", grid[i][j].getFinalCost());
+                } else {
+                    System.out.print("BL  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
-        testPathFinder(5, 5, 0, 0, 2, 4, new int[][]{{1,0},{1,1}, {0,3},{2,3}, {3,2}, {4,1}});
+    /**
+     * initializes a grid with heuristic costs
+     * @param grid the map
+     * @param width the width of the map
+     * @param height the height of the map
+     */
+    private static void initializeMap(Cell[][] grid, int width, int height) {
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                grid[i][j] = new Cell(i, j);
+                grid[i][j].setHeuristicCost(Math.abs(i - targetX) + Math.abs(j - targetY));
+            }
+        }
+    }
+
+    /**
+     * prints the map to the shell
+     * @param width width of the map
+     * @param height height of the map
+     * @param startX x-coordinate of the starting point
+     * @param startY y-coordinate of the starting point
+     * @param targetX x-coordinate of the target location
+     * @param targetY y-coordinate of the target location
+     */
+    private static void printMap(int width, int height, int startX, int startY, int targetX, int targetY) {
+        System.out.println("Grid: ");
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                if (i == startX && j == startY) {
+                    System.out.print("SO  ");
+                } else if (i == targetX && j == targetY) {
+                    System.out.print("TG  ");
+                } else if (grid[i][j] != null) {
+                    System.out.printf("%-3d ", 0);
+                } else {
+                    System.out.print("BL  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new PathFinder(5, 5, 0, 0, 0, 4, new int[][]{{2,2}, {1,1}, {3,3}, {0, 3}});
+        new PathFinder(5, 5, 0, 0, 2, 4, new int[][]{{1,0},{1,1}, {0,3},{2,3}, {3,2}, {4,1}});
     }
 }
