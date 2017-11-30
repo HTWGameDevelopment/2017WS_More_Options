11c47d18927c59bc1253a48af22f4382b922d74c
Frankiejones <Andaloria@gmx.de>
2017-11-30 14:33:49 +0100

    Node class

--- core/src/com/moreoptions/prototype/gameEngine/data/ai/Node.java
+++ core/src/com/moreoptions/prototype/gameEngine/data/ai/Node.java
@@ -1,7 +1,56 @@
 package com.moreoptions.prototype.gameEngine.data.ai;
 
+import com.badlogic.ashley.core.Entity;
+
 /**
  * Created by Andreas on 30.11.2017.
  */
+
 public class Node {
+
+    private float x;
+    private float y;
+
+    private boolean blocked;
+
+    private Entity blocker;
+
+    public Node(float x, float y) {
+        this.x = x;
+        this.y = y;
+        this.blocked = false;
+        this.blocker = null;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public Entity getBlocker() {
+        return blocker;
+    }
+
+    public void setBlocker(Entity blocker) {
+        this.blocker = blocker;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
 }
